b3db995fe75f2614857c446f08cf7d85928c1609
Eduardo Andrade <edu2andrade@gmail.com>
2022-01-02 16:56:59 +0100

    conditionally listen the PORT

--- controllers/image.js
+++ controllers/image.js
@@ -1,31 +1,31 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
-  apiKey: process.env.API_CLARIFAI // put your API Key here! It's free ;)
+  apiKey: process.env.API_CLARIFAI, // put your API Key here! It's free ;)
 });
 
 const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
+    .then((data) => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('Unable to connect with API...'))
-}
+    .catch((err) => res.status(400).json("Unable to connect with API...", err));
+};
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('Unable to get entries...'))
-}
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("Unable to get entries...", err));
+};
 
 module.exports = {
   handleImage, // ES6
-  handleApiCall
-}
-
+  handleApiCall,
+};
--- server.js
+++ server.js
@@ -1,30 +1,27 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+const knex = require("knex");
 
-const signin = require('./controllers/signin');
-const register = require('./controllers/register');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const signin = require("./controllers/signin");
+const register = require("./controllers/register");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
-
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
 
-    // host : '127.0.0.1', // Localhost --> 127.0.0.1 / heroku --> postgresql-encircled-84512
-    // port : 5432,
-    // user : 'postgres', // pgadmi --> postgres
-    // password : 'test',
-    // database : 'face-recognition'
-
-
-  }
+    // host: "127.0.0.1", // Localhost --> 127.0.0.1 / heroku --> postgresql-encircled-84512
+    // port: 5432,
+    // user: "postgres", // pgadmin --> postgres
+    // password: "test",
+    // database: "face-recognition",
+  },
 });
 
 const app = express();
@@ -34,23 +31,40 @@ app.use(cors()); // cors
 
 // Designing our routes:
 // / --> res = Server is ok!
-app.get('/', (req, res) => { res.send('Success!') })
+app.get("/", (req, res) => {
+  res.send("Success!");
+});
 
 // /signin --> POST = success/fail
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) }) // dependency injection (db, bcrypt)
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+}); // dependency injection (db, bcrypt)
 
 // /register --> POST = new user
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) // dependency injection (db, bcrypt)
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+}); // dependency injection (db, bcrypt)
 
 // /profile/:userId --> GET = user
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) }) // dependency injection (db)
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+}); // dependency injection (db)
 
 // /image --> PUT --> user
-app.put('/image', (req, res) => { image.handleImage(req, res, db) }) // dependency injection (db)
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) }) // calls another function inside image.js
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+}); // dependency injection (db)
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+}); // calls another function inside image.js
 
 // listening the right port...
-app.listen(process.env.PORT || 3000, () => { console.log(`Server is running on ${process.env.PORT}`) })
+app.listen(process.env.PORT || 3000, () => {
+  process.env.PORT
+    ? console.log(`Server is running on port ${process.env.PORT}`)
+    : console.log(`Server is running on port 3000`);
+});
+
 // This will work too:
 // let PORT = process.env.PORT;
 // if (PORT == null || PORT == "") {
